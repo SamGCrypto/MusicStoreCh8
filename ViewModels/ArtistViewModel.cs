77f9d29ffb9cdac800e961f6ad8aac1b18c0bf06
samgcrypto <skyrealmoblivion@gmail.com>
2023-10-29 19:53:12 -0400

    Add project files.

---
+++ ViewModels/ArtistViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MusicStore.Models;
+namespace MusicStore.ViewModels
+{
+    public class ArtistViewModel
+    {
+        public List<Album> Album { get; set; } = new List<Album>();
+        public List<Artist> Artist { get; set;} = new List<Artist>();
+    }
+}
